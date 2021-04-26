b1a3e345f4c7c7fcac03cfbb7f51e9fe1a245e32
fallen0253 <51790192+fallen0253@users.noreply.github.com>
2021-04-26 18:16:02 +0900

    FragmentLivingRoom.java XML 자바 연동 FragmentKitchen.java XML 자바 연동 FragmentRoom.java XML 자바 연동

--- app/src/main/java/com/example/homeplus/FragmentRoom.java
+++ app/src/main/java/com/example/homeplus/FragmentRoom.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
@@ -29,14 +30,16 @@ public class FragmentRoom extends Fragment {
                              Bundle savedInstanceState) {
         /*상현 2021-04-21 뷰 위젯 생성 후 프레그먼트를 인플레이트 한다.*/
         View fragroom = inflater.inflate(R.layout.fragment_room,container,false);
-        TextView text1 = fragroom.findViewById(R.id.text1);
+        TextView tvRED,tvGREEN,tvBLUE;
+        ProgressBar pbRED,pbGREEN,pbBLUE;
+        tvRED = fragroom.findViewById(R.id.tvRED);
+        tvGREEN = fragroom.findViewById(R.id.tvGREEN);
+        tvBLUE = fragroom.findViewById(R.id.tvBLUE);
+        pbRED = fragroom.findViewById(R.id.pbRED);
+        pbGREEN = fragroom.findViewById(R.id.pbGREEN);
+        pbBLUE = fragroom.findViewById(R.id.pbBLUE);
         /*상현 2021-04-21 텍스트뷰 선택시 발생할 이벤트 작동 제대로 되나 실험*/
-        text1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text1.setTextColor(0xAA1e6de0);
-            }
-        });
+
         return fragroom;
     }
 }
